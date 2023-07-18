cabbf531ac59cf5dca08f30f574fc489df267822
valcoellar <valcoellar@gmail.com>
2023-07-18 17:08:29 -0600

    init

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.post("/create_preference" , function (req, res) {
       payer: {
       name: "Lalo",
       surname: "Landa",
-    email: "test_user_94708656@testuser.com",
+   // email: "test_user_94708656@testuser.com",
       //email: "direccion@gruponucleon.com",
       phone: {
         area_code: "52",
