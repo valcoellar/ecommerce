4b772d1a3c45024623ec94177fc8b906ca43aa9e
valcoellar <valcoellar@gmail.com>
2023-07-18 18:03:13 -0600

    init

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const mercadopago = require("mercadopago");
 
 // Agrega credenciales
 mercadopago.configure({
-  access_token: "APP_USR-2926550097213535-092911-5eded40868803c83f12e9eef1afa99fa-1160956296",
+  access_token: "TEST-2926550097213535-092911-9f95049df86420b3055add7ae6e0e56e-1160956296",
   "x-integrator_id": "dev_24c65fb163bf11ea96500242ac130004",
+  integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
 });
 
-mercadopago.configurations.setAccessToken("APP_USR-2926550097213535-092911-5eded40868803c83f12e9eef1afa99fa-1160956296");
+mercadopago.configurations.setAccessToken("TEST-2926550097213535-092911-9f95049df86420b3055add7ae6e0e56e-1160956296");
 
 // ------------------------------
 
